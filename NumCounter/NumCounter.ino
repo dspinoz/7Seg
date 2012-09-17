8f7d67d944016a1f542343ae664a4408b7731714
dspinoz <dspinoz@gmail.com>
2012-09-17 13:20:26 +0930

    flash all leds in the display

--- NumCounter/NumCounter.ino
+++ NumCounter/NumCounter.ino
@@ -2,17 +2,23 @@
 //Use the 7 segment display to count a series of numbers
 //dspinoz@spinoz.com.au
  
-int pin = 2;
+int pins[] = {2, 3, 4, 5, 6, 7, 8, 9};
 
 void setup() {                
   // initialize the digital pin as an output.
   // Pin 13 has an LED connected on most Arduino boards:
-  pinMode(pin, OUTPUT);     
+  for (int i = 0; i< 8; i++)
+  {
+    pinMode(pins[i], OUTPUT);
+  }  
 }
 
 void loop() {
-  digitalWrite(pin, HIGH);   // set the LED on
-  delay(1000);              // wait for a second
-  digitalWrite(pin, LOW);    // set the LED off
-  delay(1000);              // wait for a second
+  for (int i = 0; i < 8; i++)
+  {
+    digitalWrite(pins[i], LOW);   // set the LED on
+    delay(1000);              // wait for a second
+    digitalWrite(pins[i], HIGH);    // set the LED off
+    delay(1000);              // wait for a second
+  }
 }
