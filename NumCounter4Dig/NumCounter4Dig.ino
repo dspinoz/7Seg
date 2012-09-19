c5467d281299d9a355939b498d7f2afbdbacc111
dspinoz <dspinoz@gmail.com>
2012-09-19 22:34:50 +0930

    new project

---
+++ NumCounter4Dig/NumCounter4Dig.ino
@@ -0,0 +1,12 @@
+
+//NumCounter4Dig
+//Use a 4 digit 7 segment display to count a series of numbers
+//Because its a common cathode display, HIGH = ON and LOW = OFF.
+//Digit pins must be multiplexed!
+//dspinoz@spinoz.com.au
+
+void setup() { 
+}
+
+void loop() {
+}
