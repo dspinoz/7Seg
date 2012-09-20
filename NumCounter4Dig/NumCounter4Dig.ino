ca4b08a5ab6505f3d4d020f8511ba046067ddced
dspinoz <dspinoz@gmail.com>
2012-09-20 22:25:02 +0930

    each digit counts from 0-9

--- NumCounter4Dig/NumCounter4Dig.ino
+++ NumCounter4Dig/NumCounter4Dig.ino
@@ -21,6 +21,24 @@
 #define pDIG3 9
 #define pDIG4 12
 
+int p7SegDisplayNum = 8;
+int p7SegDisplay[] = {pA, pB, pC, pD, pE, pF, pG, pDP};
+
+// Pin values for the 7 segment display
+char displayPinVals[][8] = { 
+  //A     B     C     D     E     F     G     DP
+  {HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, LOW , LOW }, //0
+  {LOW , HIGH, HIGH, LOW , LOW , LOW , LOW , LOW }, //1
+  {HIGH, HIGH, LOW , HIGH, HIGH, LOW , HIGH, LOW }, //2
+  {HIGH, HIGH, HIGH, HIGH, LOW , LOW , HIGH, LOW }, //3
+  {LOW , HIGH, HIGH, LOW , LOW , HIGH, HIGH, LOW }, //4
+  {HIGH, LOW , HIGH, HIGH, LOW , HIGH, HIGH, LOW }, //5
+  {HIGH, LOW , HIGH, HIGH, HIGH, HIGH, HIGH, LOW }, //6
+  {HIGH, HIGH, HIGH, LOW , LOW , LOW , LOW , LOW }, //7
+  {HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, LOW }, //8
+  {HIGH, HIGH, HIGH, LOW , LOW , HIGH, HIGH, LOW }  //9
+};
+
 void setup() { 
   
   pinMode(pDIG1, OUTPUT);
@@ -29,6 +47,13 @@ void setup() {
   pinMode(pDIG4, OUTPUT);
   
   pinMode(pA, OUTPUT);
+  pinMode(pB, OUTPUT);
+  pinMode(pC, OUTPUT);
+  pinMode(pD, OUTPUT);
+  pinMode(pE, OUTPUT);
+  pinMode(pF, OUTPUT);
+  pinMode(pG, OUTPUT);
+  pinMode(pDP, OUTPUT);
   
   //default - nothing on display
   digitalWrite(pDIG1, HIGH);
@@ -36,6 +61,13 @@ void setup() {
   digitalWrite(pDIG3, HIGH);
   digitalWrite(pDIG4, HIGH);
   digitalWrite(pA, LOW);
+  digitalWrite(pB, LOW);
+  digitalWrite(pC, LOW);
+  digitalWrite(pD, LOW);
+  digitalWrite(pE, LOW);
+  digitalWrite(pF, LOW);
+  digitalWrite(pG, LOW);
+  digitalWrite(pDP, LOW);
   
 }
 
@@ -83,7 +115,16 @@ void loop()
   
   showDigit++;
   
-  digitalWrite(pA, HIGH);    
-  delay(40);  
+  //Vals from 0 - 9
+  for (int num = 0; num < 10; num++)
+  {
+    //Set values for pins
+    for (int pin = 0; pin < p7SegDisplayNum; pin++)
+    {
+      digitalWrite(p7SegDisplay[pin], displayPinVals[num][pin]);
+    }
+      
+    delay(100);
+  }
   
 }
