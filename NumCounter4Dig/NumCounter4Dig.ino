f0b1e299e471a76c4dbec3164fa91d1113452aa8
dspinoz <dspinoz@gmail.com>
2012-09-20 21:58:10 +0930

    iterate through digits

--- NumCounter4Dig/NumCounter4Dig.ino
+++ NumCounter4Dig/NumCounter4Dig.ino
@@ -5,8 +5,85 @@
 //Digit pins must be multiplexed!
 //dspinoz@spinoz.com.au
 
+//PINS for the 7-segments
+#define pA 2
+#define pB 1
+#define pC 10
+#define pD 11
+#define pE 7
+#define pF 4
+#define pG 5
+#define pDP 3
+
+//PINS for the digits
+#define pDIG1 6
+#define pDIG2 8
+#define pDIG3 9
+#define pDIG4 12
+
 void setup() { 
+  
+  pinMode(pDIG1, OUTPUT);
+  pinMode(pDIG2, OUTPUT);
+  pinMode(pDIG3, OUTPUT);
+  pinMode(pDIG4, OUTPUT);
+  
+  pinMode(pA, OUTPUT);
+  
+  //default - nothing on display
+  digitalWrite(pDIG1, HIGH);
+  digitalWrite(pDIG2, HIGH);
+  digitalWrite(pDIG3, HIGH);
+  digitalWrite(pDIG4, HIGH);
+  digitalWrite(pA, LOW);
+  
 }
 
-void loop() {
+int showDigit = 0;
+
+void loop() 
+{
+  if (showDigit >= 4)
+  {
+    showDigit=0; 
+  }
+  
+  switch (showDigit)
+  {
+    case 0:
+      digitalWrite(pDIG1, LOW); 
+      digitalWrite(pDIG2, HIGH); 
+      digitalWrite(pDIG3, HIGH);
+      digitalWrite(pDIG4, HIGH); 
+      break;
+    case 1:
+      digitalWrite(pDIG1, HIGH); 
+      digitalWrite(pDIG2, LOW); 
+      digitalWrite(pDIG3, HIGH);
+      digitalWrite(pDIG4, HIGH); 
+      break;
+    case 2:
+      digitalWrite(pDIG1, HIGH); 
+      digitalWrite(pDIG2, HIGH); 
+      digitalWrite(pDIG3, LOW);
+      digitalWrite(pDIG4, HIGH); 
+      break;
+    case 3:
+      digitalWrite(pDIG1, HIGH); 
+      digitalWrite(pDIG2, HIGH); 
+      digitalWrite(pDIG3, HIGH);
+      digitalWrite(pDIG4, LOW); 
+      break;
+    default:
+      digitalWrite(pDIG1, HIGH); 
+      digitalWrite(pDIG2, HIGH); 
+      digitalWrite(pDIG3, HIGH);
+      digitalWrite(pDIG4, HIGH); 
+  }
+  
+  showDigit++;
+  
+  digitalWrite(pA, HIGH);    
+  delay(40);  
+  
 }
