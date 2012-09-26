c618c1fa6ac6e3dfed80df2308e2ba37807cb36c
dspinoz <dspinoz@gmail.com>
2012-09-26 23:52:10 +0930

    count from 0-9

--- NumCounterShiftRegister/NumCounterShiftRegister.ino
+++ NumCounterShiftRegister/NumCounterShiftRegister.ino
@@ -12,42 +12,45 @@ int clockPin = 12;
 ////Pin connected to DS of 74HC595
 int dataPin = 11;
 
+//bits to set in order to display a number on the display
+char displayPinVals[10] = { 
+  0x3F, //0
+  0x6, //1
+  0x5B, //2
+  0x4F, //3
+  0x66, //4
+  0x6D, //5
+  0x7D, //6
+  0x07, //7
+  0x7F, //8
+  0x67 //9
+};
+
 void setup() 
 {
   //set pins to output so you can control the shift register
   pinMode(latchPin, OUTPUT);
   pinMode(clockPin, OUTPUT);
   pinMode(dataPin, OUTPUT);
-  pinMode(1, OUTPUT);
 }
 
 void loop() 
 {
-  delay(1000);
-  digitalWrite(1, HIGH);
-  
-  // take the latchPin low so 
-  // the LEDs don't change while you're sending in bits
-  digitalWrite(latchPin, LOW);
-  
-  // shift out some bits
-  shiftOut(dataPin, clockPin, MSBFIRST, 0); 
-  
-  //take the latch pin high so the LEDs will light up:
-  digitalWrite(latchPin, HIGH);
-  
-  delay(1000);
-  
-  
-  // take the latchPin low so 
-  // the LEDs don't change while you're sending in bits
-  digitalWrite(latchPin, LOW);
-  
-  // shift out some bits
-  shiftOut(dataPin, clockPin, MSBFIRST, 7); 
-  
-  //take the latch pin high so the LEDs will light up:
-  digitalWrite(latchPin, HIGH);
-  
-  digitalWrite(1, LOW);
+  //Vals from 0 - 9
+  for (int num = 0; num < 10; num++)
+  {
+    // take the latchPin low so 
+    // the LEDs don't change while you're sending in bits
+    digitalWrite(latchPin, LOW);
+    
+    // shift out some bits
+    //HIGH = off, LOW = on
+    //binary not operator (~) to flip the bits
+    shiftOut(dataPin, clockPin, MSBFIRST, ~displayPinVals[num]); 
+    
+    //take the latch pin high so the LEDs will turn off
+    digitalWrite(latchPin, HIGH);
+    
+    delay(250);
+  }
 }
