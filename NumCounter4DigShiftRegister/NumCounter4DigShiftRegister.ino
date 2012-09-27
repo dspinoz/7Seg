e16e4b0ef695c220c117c38ec585ae85ce0c7590
dspinoz <dspinoz@gmail.com>
2012-09-27 21:52:21 +0930

    new project

---
+++ NumCounter4DigShiftRegister/NumCounter4DigShiftRegister.ino
@@ -0,0 +1,15 @@
+//NumCounter
+//Use a 4 Digit 7 segment display to count a series of numbers
+//Reduce the number of pins from the arduino to the display
+//  by using an 8 bit shift register - 74HC595.
+//dspinoz@spinoz.com.au
+//http://arduino.cc/en/Tutorial/ShftOut11
+
+void setup() 
+{
+}
+
+void loop() 
+{
+}
+
