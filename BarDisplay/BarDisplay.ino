4c784ae621b131bfad9e9fc05dfbfdb7cddd27c4
dspinoz <dspinoz@gmail.com>
2012-11-29 23:30:16 +1030

    display speed from GPS

--- BarDisplay/BarDisplay.ino
+++ BarDisplay/BarDisplay.ino
@@ -3,12 +3,21 @@
                connected to digital pins
 */
 
+#include <TinyGPS.h>
+
+TinyGPS gps;
+
+#define led 13
 #define pStartLED 2
 #define pEndLED 7
 
 void setup() 
 {
-  Serial.begin(9600);
+  Serial.begin(57600);
+  
+  pinMode(led, OUTPUT);
+  digitalWrite(led, LOW);
+  
   for (size_t i = pStartLED; i <= pEndLED; i++)
   {
     pinMode(i, OUTPUT);
@@ -30,48 +39,61 @@ void show(size_t num)
   }   
 }
 
-void ledPerc(size_t p, size_t minp, size_t maxp)
+void showFromSpeed(float i)
 {
-  if (p < minp)
+  int leds = 0;
+  
+  if (i >= 20)
   {
-     show(0);
-    return; 
+    //green
+    leds++;
   }
   
-  int ledMax = pEndLED - pStartLED;
+  if (i >= 60)
+  {
+    //green
+    leds++;
+  }
   
-  float incr = (float)ledMax / (maxp-minp);
+  if (i >= 80)
+  {
+    //green
+    leds++;
+  }
   
-  int num = (p-minp) * incr;
+  if (i >= 90)
+  {
+    //green
+    leds++;
+  }
   
-  Serial.print(ledMax);
-  Serial.print(" ");
-  Serial.print(p);
-  Serial.print(" ");
-  Serial.print((p-minp));
-  Serial.print(" ");
-  Serial.print(incr);
-  Serial.print("  ");
-  Serial.print(num);
-  Serial.println();
+  if (i >= 100)
+  {
+    //yellow
+    leds++;
+  }
   
-  show(num);
+  if (i >= 110)
+  {
+    //red
+    leds++; 
+  }
   
+  show(leds);
 }
-
-#define maxval 100
-size_t i = 0;
 
 void loop() 
 {
-  i++;
-  if (i>maxval+30)
+  digitalWrite(led, LOW);
+  
+  while (Serial.available())
   {
-    i=1;
+    digitalWrite(led, LOW);
+    char c = Serial.read();
+    if (gps.encode(c))
+    {
+      digitalWrite(led, HIGH);
+      showFromSpeed((int)gps.f_speed_kmph());
+    }
   }
-  
-  ledPerc(i, 50, maxval);
-  
-  delay(100);
-  
 }
